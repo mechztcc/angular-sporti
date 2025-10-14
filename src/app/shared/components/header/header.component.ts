bce110533c0f5718983cc311b438e9eb107e1429
mechztcc <dojathan@gmail.com>
2025-10-14 09:10:03 -0300

    feat: add header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faChevronDown, faGlobe, faIndent, faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  icons: any = {
+    global: faGlobe,
+    user: faUser,
+    menu: faBars,
+    search: faMagnifyingGlass,
+    down: faChevronDown,
+    ident: faIndent
+  }
 }
